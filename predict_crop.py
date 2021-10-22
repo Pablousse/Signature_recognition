47f637ee48fb30eb74de88d7872663b3f8f84943
Pablousse <durieux.arnaud@hotmail.com>
2021-10-22 15:50:47 +0200

    added predict file for crop attempt

--- predict_crop.py
+++ predict_crop.py
@@ -1,34 +1,14 @@
 from tensorflow import keras
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-import os
 import csv
 
 
-print("------------------------TEST 1------------------------")
+model = keras.models.load_model("assets/crop_model")
 
-model = keras.models.load_model("assets/a4_model")
-
-print("------------------------TEST 456------------------------")
-
-train_datagen = ImageDataGenerator(rescale=1. / 55)
-
-# # Flow training images in batches of 20 using train_datagen generator
-# train_generator = train_datagen.flow_from_directory(
-#     # This is the source directory for training images
-#     "assets/test",
-#     # All images will be resized to 150x150
-#     target_size=(150, 150),
-#     # Define how big are gonna be your batch.
-#     batch_size=20,
-#     # Since we use binary_crossentropy loss, we need binary labels
-#     class_mode='binary'
-# )
-# print(model.predict(train_generator))
-
-print("------------------------TEST 3------------------------")
+train_datagen = ImageDataGenerator(rescale=1. / 255)
 
 generator = train_datagen.flow_from_directory(
-        "assets/Prediction",
+        "assets/prediction_cropped",
         target_size=(150, 150),
         batch_size=20,
         class_mode="binary",  # this means our generator will only yield batches of data, no labels
@@ -42,7 +22,14 @@ predictions = list(zip(bottleneck_features_train, generator.filenames))
 to_csv_format = []
 
 for prediction in predictions:
-    to_csv_format.append([os.path.splitext(prediction[1])[0].replace("test/", ""), prediction[0][0]])
+    filename = prediction[1].replace("test/", "").split("_")[0]
+    if len(to_csv_format) > 0:
+        if to_csv_format[-1][0] == filename and prediction[0][0] == 1:
+            to_csv_format[-1][1] = 1
+        elif not to_csv_format[-1][0] == filename:
+            to_csv_format.append([filename, prediction[0][0]])
+    else:
+        to_csv_format.append([filename, prediction[0][0]])
 
 for line in to_csv_format:
     print(line)
